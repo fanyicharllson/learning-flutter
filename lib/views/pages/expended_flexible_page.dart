b4054adec741d37405ba12574e9e31c7d7e8253e
fanyicharllson <fanyicharllson1@gmail.com>
2025-08-26 16:44:53 -0700

    Added expandable and flexible widget

--- lib/views/pages/expended_flexible_page.dart
+++ lib/views/pages/expended_flexible_page.dart
@@ -7,7 +7,23 @@ class ExpendedFlexiblePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 4, //? will take 4 parts of the total 6 parts
+            child: Container( //! Expnaded best used in rows and cols...
+            color: Colors.teal,
+          )
+          ),
+          Expanded(
+            flex: 2, //? will two times the space of the first one
+            child: 
+            Container( //! Expnaded best used in rows and cols...
+            color: Colors.teal,
+          )
+          ),
+        ],
+      ),
     );
   }
 }
